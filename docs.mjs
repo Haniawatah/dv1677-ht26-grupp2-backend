e4f3dc9aceef60f231fcdf854673e6491aa28101
Tommy Johannesson <atjohannesson@gmail.com>
2026-09-24 17:49:13 +0200

    refactor: anpassat funktionerna till att arbeta mot mongoDB

--- docs.mjs
+++ docs.mjs
@@ -1,11 +1,26 @@
-import db from './db/database.mjs';
+import { connectToDatabase, db } from './db/database.mjs';
+import { ObjectId } from 'mongodb';
 
 const docs = {
     getAll: async function getAll() {
-        return db.prepare('SELECT * FROM documents').all();
+        connectToDatabase();
+        let temp = await db.collection("documents").find().toArray();
+        console.log(temp);
+        return temp;
+        // return await db.collection("documents").find().toArray();
+        // return db.prepare('SELECT * FROM documents').all();
     },
     getOne: async function getOne(id) {
-        return db.prepare('SELECT * FROM documents WHERE id = ?').get(id) || {};
+        connectToDatabase();
+        console.log("före" + typeof(id));
+        let temp = await db
+            .collection("documents")
+            .findOne({ _id: new ObjectId(id) });
+        // console.log("efter" + typeof(_id));
+        console.log(temp);
+        return temp;
+        // return await db.collection("documents").find({ _id: new ObjectId(id) }).toArray();
+        // return db.prepare('SELECT * FROM documents WHERE id = ?').get(id) || {};
     },
     addOne: async function addOne(body) {
         const result = db.prepare(
@@ -14,9 +29,15 @@ const docs = {
         return { lastID: result.lastInsertRowid };
     },
     updateOne: async function updateOne(id, body) {
-        return db.prepare(
-            'UPDATE documents SET title = ?, content = ? WHERE id = ?'
-        ).run(body.title, body.content, id);
+        const { title, content } = body;
+
+        await db.collection("documents").updateOne(
+            { _id: new ObjectId(id) },
+            { $set: {title, content } }
+        );
+        // return db.prepare(
+        //     'UPDATE documents SET title = ?, content = ? WHERE id = ?'
+        // ).run(body.title, body.content, id);
     }
 };
 
